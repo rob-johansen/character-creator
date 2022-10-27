f40f298fc74c5560338e9ada250e366b8372228b
Rob Johansen <rob.johansen@gmail.com>
2022-10-26 22:38:37 -0600

    Solve character workshop

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import styles from './App.module.css'
 
 function App() {
   return (
     <>
+      <div className={styles.bar} />
       <CharacterEditor />
       <Footer />
     </>
